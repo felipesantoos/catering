e254158f31a6137b773bd6a8ecde27bcdbf73ce7
felipesantosds <felipedssantos.dev@gmail.com>
2021-11-12 13:57:13 -0300

    Add header responsivity

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,10 +1,52 @@
+import { createRef, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 import styles from "../styles/components/Header.module.scss";
 
 function Header() {
+    const [isMenuShowed, setIsMenuShowed] = useState(false);
+    const menuRef = createRef<HTMLDivElement>();
+    const headerRef = createRef<HTMLElement>();
+
+    function showMenu() {
+        if (menuRef.current) {
+            if (isMenuShowed) {
+                menuRef.current.style.display = "none";
+                setIsMenuShowed(false);
+            } else {
+                menuRef.current.style.display = "flex";
+                setIsMenuShowed(true);
+            }
+        }
+    }
+
+    const mql = window.matchMedia("(max-width: 670px)");
+
+    function setHeader(event: any) {
+        if (event.matches) {
+            if (headerRef.current && menuRef.current) {
+                headerRef.current.style.flexDirection = "column";
+                menuRef.current.style.display = "none";
+                setIsMenuShowed(false);
+            }
+        } else {
+            if (headerRef.current && menuRef.current) {
+                headerRef.current.style.flexDirection = "row";
+                menuRef.current.style.display = "flex";
+                setIsMenuShowed(true);
+            }
+        }
+    }
+
+    mql.addEventListener("change", setHeader);
+
     return (
-        <header className={styles.container}>
-            <h4><a href="#">Gourmet au Catering</a></h4>
+        <header ref={headerRef} className={styles.container}>
             <div>
+                <h4><a href="#">Gourmet au Catering</a></h4>
+                <GiHamburgerMenu onClick={showMenu} />
+            </div>
+            <div ref={menuRef}>
                 <ul>
                     <li><a href="#about">About</a></li>
                     <li><a href="#menu">Menu</a></li>
